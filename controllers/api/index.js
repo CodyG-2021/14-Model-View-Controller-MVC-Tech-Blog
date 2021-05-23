bc48c64f6362350ef4127089da3d79c9765ea251
Cody <gallagher2021@gmail.com>
2021-05-23 15:46:52 -0600

    Added blog routes api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
-// const dashboardRoutes = require('./dashBoardRoutes');
 
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
-// router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
 
