472bf9cc3533c345c7849a480e6c50f0dc13e6fa
Cody <gallagher2021@gmail.com>
2021-05-23 18:44:33 -0600

    Added more to the js public files updated handlebars to get login working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,97 @@
 const router = require('express').Router();
-const { User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { User, Blog, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    res.render('homepage');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const blogData = await Blog.findAll({
+			include: [{
+				model: User,
+				attributes: ['username'],
+			},],
+		});
+
+		const blogs = blogData.map((blog) => blog.get({
+			plain: true
+		}));
+
+		res.render('homepage', {
+			blogs,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
+router.get('/blog/:id', async (req, res) => {
+	try {
+		const blogData = await Blog.findByPk(req.params.id, {
+			include: [
+				{
+					model: User,
+					attributes: ['username'],
+				}, {
+					model: Comment,
+					include: [
+						User
+					]
+				}
+			],
+		});
+
+		const blog = blogData.get({
+			plain: true
+		});
+
+		res.render('blog', {
+			...blog,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+	try {
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: {
+				exclude: ['password']
+			},
+			include: [{
+				model: Blog
+			}],
+		});
+
+		const user = userData.get({
+			plain: true
+		});
+
+		res.render('dashboard', {
+			...user,
+			logged_in: true
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/login', (req, res) => {
-  res.render('login');
+	if (req.session.logged_in) {
+		res.redirect('/dashboard');
+		return;
+	}
+
+	res.render('login');
 });
 
-router.get('/SignUp', (req, res) => {
-  res.render('SignUp');
+router.get('/signup', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/dashboard');
+		return;
+	}
+	res.render('signup');
 });
 
 module.exports = router;
